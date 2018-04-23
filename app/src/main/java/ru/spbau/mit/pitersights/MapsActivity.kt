67aac18f99a59a06b53dac8daa62743a82d337cc
Andrey Kravtsun <avkravtsun@gmail.com>
2018-04-23 03:23:42 +0300

    Small fixes for compileability.

--- app/src/main/java/ru/spbau/mit/pitersights/MapsActivity.kt
+++ app/src/main/java/ru/spbau/mit/pitersights/MapsActivity.kt
@@ -10,13 +10,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
