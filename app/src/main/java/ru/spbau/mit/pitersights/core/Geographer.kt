f48322293016c59d83b2267d0ec671e087e8cd9a
Angelika <anzhelikamukhina@yandex.ru>
2018-05-22 01:34:03 +0300

    left and right neighbors getters implementations in Geographer

--- app/src/main/java/ru/spbau/mit/pitersights/core/Geographer.kt
+++ app/src/main/java/ru/spbau/mit/pitersights/core/Geographer.kt
@@ -64,10 +64,34 @@ class Geographer {
     }
 
     fun getLeftNearSights(player: Player, neighbors: Map<Sight, Float>): Map<Sight, Float> {
-        return neighbors
+        val playerLocation = player.geoLocation
+        val playerViewDirection = playerLocation!!.bearing
+        val leftNeighbors = mutableMapOf<Sight, Float>()
+        for (sight in neighbors) {
+            val heading = computeHeading(
+                    LatLng(playerLocation.latitude, playerLocation.longitude),
+                    sight.key.geoPosition
+            )
+            if (playerViewDirection - heading < 0) {
+                leftNeighbors[sight.key] = sight.value
+            }
+        }
+        return leftNeighbors
     }
 
     fun getRightNearSights(player: Player, neighbors: Map<Sight, Float>): Map<Sight, Float> {
-        return neighbors
+        val playerLocation = player.geoLocation
+        val playerViewDirection = playerLocation!!.bearing
+        val rightNeighbors = mutableMapOf<Sight, Float>()
+        for (sight in neighbors) {
+            val heading = computeHeading(
+                    LatLng(playerLocation.latitude, playerLocation.longitude),
+                    sight.key.geoPosition
+            )
+            if (heading - playerViewDirection < 0) {
+                rightNeighbors[sight.key] = sight.value
+            }
+        }
+        return rightNeighbors
     }
 }
